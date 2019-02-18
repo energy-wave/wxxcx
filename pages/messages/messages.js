b11093bd8fc41510ea2bdb896c0f3754727b2724
energywave <515023802@qq.com>
2019-02-18 18:54:32 +0800

    2019年2月18日 18:54:28

--- pages/messages/messages.js
+++ pages/messages/messages.js
@@ -7,60 +7,73 @@ Page({
   data: {
 
   },
-
+  sendData(e) {
+    console.log('form发生了submit事件，携带数据为：', e.detail.value)
+    wx.request({
+      url: 'https://www.energywave.top:3054/message/new',
+      data: {
+        name: e.detail.value.name,
+        msg: e.detail.value.msg
+      },
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      method: 'POST'
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
